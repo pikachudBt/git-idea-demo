95d3e32e10990d4d3ba33304d92aa1e705c5dea4
yhm <yaohm7788@163.com>
2024-03-29 19:39:18 +0800

    hello1

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("Hello1");
+        System.out.println("Hello2");
     }
 }
